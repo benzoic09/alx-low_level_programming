8cb010d71b9ea8cb2acf54ed81db75eca5f1096f
benzoic09 <benzoic09@gmail.com>
2023-06-15 13:47:11 -0700

    pushing c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-str puts('Programming is like building a multilingual puzzle')
+#include <stdio.h>
+/**
+ * start of code
+ */
+int main(void)
+{
+str puts"\"Programming is like building a multilingual puzzle",\n)
+	return(0)
+}
