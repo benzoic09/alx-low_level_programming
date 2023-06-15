86defcf23f5e40c3096024e96a78203d12844b58
benzoic09 <benzoic09@gmail.com>
2023-06-15 14:00:48 -0700

    pushing c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * start of code
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts"\"Programming is like building a multilingual puzzle",\n);
-	return(0);
+	puts"\"Programming is like building a multilingual puzzle");
+return(0);
 }
