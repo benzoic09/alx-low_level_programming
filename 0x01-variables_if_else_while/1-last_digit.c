346a944519349378bc4723f60af3283f5aac8a2f
benzoic09 <benzoic09@gmail.com>
2023-06-16 11:07:56 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,11 +7,11 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%i is positive\n", n);
-else if (n < 0)
-printf("%i is negative\n", n);
+if (n > 5)
+printf("Last digit of %i is %i and is greater than 5\n", n, m);
+else if (m == 0)
+printf("Last digit of %i is %i and is 0\n", n, m);
 else
-printf("%i is zero\n", n);
+printf("Last digit of %i is %i and is less than 6 and not 0\n" n, m);
 return (0);
 }
