f2f69533205f5d53578c9b4f17b1275641692fc9
benzoic09 <benzoic09@gmail.com>
2023-06-16 10:54:53 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/*program will assign a random number to the variable n */
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%i is positive\n", n);
+else if (n < 0)
+printf("%i is negative\n", n);
+else
+printf("%i is zero\n", n);
+return (0);
+}
