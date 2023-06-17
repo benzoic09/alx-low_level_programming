3eb71d4b342f98d5c9bd9cfbb876cd4855fdcc1d
benzoic09 <benzoic09@gmail.com>
2023-06-17 08:23:05 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * program that prints all possible different combinations of three digits.
+ * Return: 0
+*/
+
+int main(void)
+{
+int count = 0;
+int i, j;
+
+for (i = 0; i <= 99; i++)
+{
+for (j = i; j <= 99; j++)
+{
+/* Print the first two-digit number */
+putchar('0' + i / 10);
+putchar('0' + i % 10);
+/* Print the separator */
+putchar(' ');
+/* Print the second two-digit number */
+putchar('0' + j / 10);
+putchar('0' + j % 10);
+/* Print the comma and space if it's not the last combination */
+if (++count < 4950)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
