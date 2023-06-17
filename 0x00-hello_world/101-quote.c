ee28224e95efb0473a5ca721faaa3e9989d2f368
benzoic09 <benzoic09@gmail.com>
2023-06-17 01:40:36 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main block
+ * Return: 1
+ */
 
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 57);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
 
-return 0;
+return (0);
 }
