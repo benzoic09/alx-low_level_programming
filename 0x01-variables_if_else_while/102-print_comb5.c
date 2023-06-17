aac444bccecd8666430851d5622981eeb3e6481d
benzoic09 <benzoic09@gmail.com>
2023-06-17 08:31:02 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,13 +16,13 @@ for (i = 0; i <= 99; i++)
 for (j = i; j <= 99; j++)
 {
 /* Print the first two-digit number */
-putchar('0' + i / 10);
-putchar('0' + i % 10);
+putchar('0' + (i / 10));
+putchar('0' + (i % 10));
 /* Print the separator */
 putchar(' ');
 /* Print the second two-digit number */
-putchar('0' + j / 10);
-putchar('0' + j % 10);
+putchar('0' + (j / 10));
+putchar('0' + (j % 10));
 /* Print the comma and space if it's not the last combination */
 if (++count < 4950)
 {
