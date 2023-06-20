51627454b130e2527349a22e8ba8a6172870d277
benzoic09 <benzoic09@gmail.com>
2023-06-20 12:53:52 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+print_alphabet();
+char alp
+for (alp = 'a'; alp < = 'z'; alp++)
+_putchar(alp);
+_putchar('\n');
+return (0);
+}
