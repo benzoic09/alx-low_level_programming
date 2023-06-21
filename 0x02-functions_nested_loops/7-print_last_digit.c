ce038100354f1c0d48f14f2c368830380a163301
benzoic09 <benzoic09@gmail.com>
2023-06-21 10:48:03 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,12 @@
 
 int print_last_digit(int r)
 {
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
+int last_digit;
+last_digit = r % 10;
+if (last_digit < 0)
+last_digit *= -1;
+
+_putchar('0' + last_digit);
+
+return (last_digit);
 }
