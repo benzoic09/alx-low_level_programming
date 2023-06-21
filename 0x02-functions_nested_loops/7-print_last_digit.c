dc80e768091a1a0d470b95eacfe7a154998f3038
benzoic09 <benzoic09@gmail.com>
2023-06-21 06:20:21 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - main block
+ *  program that prints all possible different combinations of three digits.
+ * Return: 0
+ */
+
+int print_last_digit(int num)
+{
+	int last = num % 10;
+
+	_putchar("last");
+	return (last);
+}
