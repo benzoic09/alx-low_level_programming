c7e29258a5c7a8ecbe5920b2f8c342d706c26ce4
benzoic09 <benzoic09@gmail.com>
2023-06-21 12:29:43 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - main block
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *  function that prints the sign of a number.
+ * Return: always 1
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
