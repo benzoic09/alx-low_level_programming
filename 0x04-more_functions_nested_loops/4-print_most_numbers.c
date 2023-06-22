d67b82cee9e576c3a904af6b82ed5fbfce2ab025
benzoic09 <benzoic09@gmail.com>
2023-06-22 15:33:54 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints 1-9 without 2&4
+ * @i: number to be inputed 
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar('0' + i);
+}
+}
+
+_putchar('\n');
+}
