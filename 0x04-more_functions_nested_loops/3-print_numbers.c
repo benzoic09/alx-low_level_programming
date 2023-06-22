f1a81642c0b0731b617ea4e68340b724e5a50f73
benzoic09 <benzoic09@gmail.com>
2023-06-22 15:22:49 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * char - number
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	char digit;
+
+for (digit = '0'; digit <= '9'; digit++)
+{
+_putchar(digit);
+}
+
+_putchar('\n');
+}
