271c1fb5f603b88c0112b125741a0bb4d8558f4f
benzoic09 <benzoic09@gmail.com>
2023-06-22 15:11:39 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply 2 numbers
+ * @a: first number
+ * @b: second numbe 
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
