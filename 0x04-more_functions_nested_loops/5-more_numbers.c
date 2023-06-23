efa30419b963aa9eca3534c56c8e04324cb280e4
benzoic09 <benzoic09@gmail.com>
2023-06-23 06:41:14 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,22 @@ void more_numbers(void)
 {
 int i, j;
 
-for (1 = 0; 1 < 10; i++)
+for (i = 0; i < 10; i++)
 {
 	for (j = 0; j <= 14; j++)
 	{
-		_putchar('0' + j);
-		_putchar('\n');
-	}
+if (j < 10)
+{
+_putchar('0' + j);
+}
+else
+{
+_putchar('0' + j / 10);
+_putchar('0' + j % 10);
+}
+}
+
+_putchar('\n');
+}
 }
 
