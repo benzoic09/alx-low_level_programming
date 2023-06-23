9524bc5184120b0e8bd690770602e674fcb4d941
benzoic09 <benzoic09@gmail.com>
2023-06-23 12:07:29 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,12 @@ int i, j;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
-	{
-if (j < 10)
 {
+if (j < 10)
 _putchar('0' + j);
-}
 else
 {
-_putchar('0' + j % 10);
+_putchar('0' + j / 10);
 _putchar('0' + j % 10);
 }
 }
@@ -28,4 +26,3 @@ _putchar('0' + j % 10);
 _putchar('\n');
 }
 }
-
