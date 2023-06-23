e054aa863ff2b00820c155fbc48fffe9c39df969
benzoic09 <benzoic09@gmail.com>
2023-06-23 12:40:10 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int len;
+
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+_putchar('_');
+}
+
+_putchar('\n');
+}
