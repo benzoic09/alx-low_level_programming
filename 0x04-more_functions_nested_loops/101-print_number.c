9a9008185190301f0c8f7a5c299880e41b3452e5
benzoic09 <benzoic09@gmail.com>
2023-06-24 08:45:21 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,20 @@
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10)
-        print_number(n / 10);
-
-    _putchar('0' + (n % 10));
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+else if (n == 0)
+{
+_putchar('0');
+return;
 }
 
+if (n / 10)
+print_number(n / 10);
+
+_putchar('0' + (n % 10));
+}
 
