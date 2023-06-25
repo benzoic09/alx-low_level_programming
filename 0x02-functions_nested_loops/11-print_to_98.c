860e6ceee9552e4d680100c0637e0c6cc81c5b42
benzoic09 <benzoic09@gmail.com>
2023-06-25 09:19:18 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,27 @@
 
 void print_to_98(int n)
 {
-int i;
-
-for (i = n; i <= 98; i++)
+if (n <= 98)
 {
-printf("%d", i);
+	while (n <= 98)
+	{
+printf("%d", n);
 
-if (i != 98)
-{
+if (n != 98)
 printf(", ");
+n++;
 }
 }
-_putchar('\n');
+else
+{
+	while (n >= 98)
+			{
+			printf("%d", n);
+			if (n != 98)
+			printf(", ");
+			n--;
+
+			}
+}
+printf("\n");
 }
