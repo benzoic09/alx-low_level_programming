b3e9486e8c33b3a5b2d18596d36df3e40d5842f8
benzoic09 <benzoic09@gmail.com>
2023-06-26 12:33:22 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * puts_half - a function that prints half of a string
  * if odd len, n = (length_of_the_string - 1) / 2
@@ -7,9 +8,9 @@
  */
 void puts_half(char *str)
 {
-	int a, n, longi;
+int a, n, longi;
 
-	longi = 0;
+long i = 0;
 
 for (a = 0; str[a] != '\0'; a++)
 		longi++;
