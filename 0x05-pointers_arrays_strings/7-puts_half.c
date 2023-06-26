771e25115dbd8d2d8e69bde8548f56de6544f34d
benzoic09 <benzoic09@gmail.com>
2023-06-26 13:04:40 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,17 +9,17 @@
 
 void puts_half(char *str)
 {
-int a, n, long i;
+int a, n, i;
 
-long i = 0;
+i = 0;
 
 	for (a = 0; str[a] != '\0'; a++)
 		longi++;
 
 n = (longi / 2);
 
-	if ((longi % 2) == 1)
-		n = ((longi + 1) / 2);
+	if ((i % 2) == 1)
+		n = ((i + 1) / 2);
 
 	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
