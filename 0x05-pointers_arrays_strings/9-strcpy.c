0b42f182570f36a754409621434fdc77930812d6
benzoic09 <benzoic09@gmail.com>
2023-06-26 13:15:02 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,7 +13,7 @@ char *_strcpy(char *dest, char *src)
 int i = 0;
 
 if (dest == NULL || src == NULL)
-return NULL;
+return (NULL);
 
 while (src[i] != '\0')
 {
@@ -23,5 +23,5 @@ i++;
 
 dest[i] = '\0';
 
-return dest;
+return (dest);
 }
