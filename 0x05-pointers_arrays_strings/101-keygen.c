1e49de43cab25f1be8b367b120c0197c0c07796a
benzoic09 <benzoic09@gmail.com>
2023-06-26 13:39:32 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,9 +4,11 @@
 
 #define PASSWORD_LENGTH 8
 
-char *generate_password() {
+char *generate_password() 
+{
     char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
-    if (password == NULL) {
+    if (password == NULL) 
+    {
         fprintf(stderr, "Error: Memory allocation failed\n");
         exit(EXIT_FAILURE);
     }
@@ -15,7 +17,8 @@ char *generate_password() {
 
     srand(time(NULL));
 
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+	int i;
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
         int index = rand() % (sizeof(charset) - 1);
         password[i] = charset[index];
     }
