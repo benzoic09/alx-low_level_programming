827d7905c20928b51d08462d47ebb7a1a87afbb2
benzoic09 <benzoic09@gmail.com>
2023-06-29 14:27:53 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+
+char *leet(char *str)
+{
+char *leet_chars = "AEOTL";
+char *leet_nums = "43071";
+int i, j;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; leet_chars[j] != '\0'; j++)
+{
+if (str[i] == leet_chars[j] || str[i] == leet_chars[j] + 32)
+{
+str[i] = leet_nums[j];
+break;
+}
+}
+}
+
+return (str);
+}
