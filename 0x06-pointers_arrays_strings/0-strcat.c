8d4375c18549f40d6c369fc8500e7c26f81eec54
benzoic09 <benzoic09@gmail.com>
2023-06-29 05:27:22 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *strcat - function name
+ *@dest: - first string
+ *@src: second string
+ * Return: always o
+ */
+
+char *strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p)
+	p++;
+	while (*srt)
+	{
+		*p = *srt;
+		p++;
+		srt;
+	}
+	*p = '\0';
+	return (dest);
+
