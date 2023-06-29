9d65e11e0028a00c425ef9ed0857ef847a2fb365
benzoic09 <benzoic09@gmail.com>
2023-06-29 14:57:19 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,28 @@
 
 void print_number(int n)
 {
+int digit;
+int is_negative = 0;
+
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+
 if (n < 0)
 {
-_putchar('-');
+is_negative = 1;
 n = -n;
 }
 
 if (n / 10 != 0)
 print_number(n / 10);
 
-_putchar('0' + (n % 10));
+digit = n % 10;
+
+if (is_negative)
+_putchar('-');
+
+_putchar('0' + digit);
 }
