bd4a02aff64895ba332c9198a89fe63bb3ee6daa
benzoic09 <benzoic09@gmail.com>
2023-06-29 11:57:01 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies a string with a specified maximum number of characters
- * @dest: The destination string
- * @src: The source string
- * @n: The maximum number of characters to copy
+ * _strcmp - compares two strings.
+ * @s1: string 1
+ * @s2: string 2
  *
- * Return: Pointer to the resulting string dest
+ * Return:combined string
  */
 
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && (*s1 == *s2)) 
+while (*s1 && (*s1 == *s2))
 {
 s1++;
 s2++;
 }
 
-return *(unsigned char*)s1 - *(unsigned char*)s2;
+return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
