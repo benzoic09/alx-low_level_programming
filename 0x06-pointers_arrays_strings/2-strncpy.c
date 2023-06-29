3ab5d47264ed472ab8df8c4fd18b6540793aad63
benzoic09 <benzoic09@gmail.com>
2023-06-29 09:13:40 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings, limiting the number of characters copied
+ * _strncpy - Copies a string with a specified maximum number of characters
  * @dest: The destination string
  * @src: The source string
  * @n: The maximum number of characters to copy
@@ -9,3 +9,15 @@
  * Return: Pointer to the resulting string dest
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
