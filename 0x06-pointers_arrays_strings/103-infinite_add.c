57044d7dafca47d374c35f40e7a1dfaa29571ee1
benzoic09 <benzoic09@gmail.com>
2023-06-30 09:11:18 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,39 +11,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum = 0;
-	int i = 0, j = 0;
+	int len1 = 0, len2 = 0, sum = 0, carry = 0;
+	int i, j;
 
 	while (n1[len1] != '\0')
 		len1++;
 	while (n2[len2] != '\0')
 		len2++;
 
-	if (len1 + 2 > size_r || len2 + 2 > size_r)
+	if (len1 > size_r - 2 || len2 > size_r - 2)
 		return (0);
 
-	len1--;
-	len2--;
-	r[size_r - 1] = '\0';
+	i = len1 - 1;
+	j = len2 - 1;
 
-	for (; len1 >= 0 || len2 >= 0 || carry; len1--, len2--, i++)
+	r[size_r - 1] = '\0';
+	for (; i >= 0 || j >= 0; i--, j--)
 	{
 		sum = carry;
-		if (len1 >= 0)
-			sum += n1[len1] - '0';
-		if (len2 >= 0)
-			sum += n2[len2] - '0';
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
 
 		carry = sum / 10;
-		r[size_r - 2 - i] = (sum % 10) + '0';
+		r[i > j ? i + 1 : j + 1] = (sum % 10) + '0';
 	}
 
-	if (i > size_r)
-		return (0);
-
-	for (; j < i; j++)
-		r[j] = r[size_r - i + j - 1];
+	if (carry)
+		r[0] = carry + '0';
+	else
+		r++;
 
 	return (r);
 }
-
