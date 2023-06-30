7bc98fdae4614414c771ae8a597195792fef33d0
benzoic09 <benzoic09@gmail.com>
2023-06-30 01:41:18 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @s: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+
+char *rot13(char *s)
+{
+	char *ptr = s;
+
+	while (*s != '\0')
+	{
+		if ((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M'))
+			  *s += 13;
+		else if ((*s >= 'n' && *s <= 'z') || (*s >= 'N' && *s <= 'Z'))
+			*s -= 13;
+		s++;
+	}
+
+return (ptr);
+}
