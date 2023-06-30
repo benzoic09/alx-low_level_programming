fc4e0c1f07218b85f917b73c2451987d3c13639b
benzoic09 <benzoic09@gmail.com>
2023-06-30 01:47:43 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,16 +9,16 @@
 
 char *rot13(char *s)
 {
-	char *ptr = s;
+char *ptr = s;
 
-	while (*s != '\0')
-	{
-		if ((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M'))
-			  *s += 13;
-		else if ((*s >= 'n' && *s <= 'z') || (*s >= 'N' && *s <= 'Z'))
-			*s -= 13;
-		s++;
-	}
+while (*s != '\0')
+{
+if ((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M'))
+*s += 13;
+else if ((*s >= 'n' && *s <= 'z') || (*s >= 'N' && *s <= 'Z'))
+*s -= 13;
+s++;
+}
 
 return (ptr);
 }
