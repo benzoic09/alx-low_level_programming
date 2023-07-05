6871061111377eebd2903c75a581ae47b7ffd599
benzoic09 <benzoic09@gmail.com>
2023-07-05 13:56:41 -0700

    0x08-recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * is_prime_helper - Helper function to check if a number is prime
+ * @n: The number to check
+ * @divisor: The current divisor to test
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+
+int is_prime_helper(int n, int divisor)
+{
+if (n < 2)
+{
+
+return (0);
+}
+
+else if (divisor * divisor > n)
+{
+return (1);
+}
+
+else if (n % divisor == 0)
+{
+return (0);
+}
+
+else
+{
+return (is_prime_helper(n, divisor + 1));
+}
+}
+
+/**
+ * is_prime_number - Checks if a number is prime
+ * @n: The number to check
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+return (is_prime_helper(n, 2));
+}
