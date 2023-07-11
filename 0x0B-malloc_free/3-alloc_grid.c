4082ed0fe2fbcc173af9515f75598b584d2cfffa
benzoic09 <benzoic09@gmail.com>
2023-07-11 13:24:21 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,13 +25,15 @@ for (i = 0; i < height; i++)
 grid[i] = malloc(sizeof(int) * width);
 if (grid[i] == NULL)
 {
+
 for (j = 0; j < i; j++)
 free(grid[j]);
 free(grid);
 return (NULL);
 }
+
 for (j = 0; j < width; j++)
-grid[i] = 0;
+grid[j] = 0;
 }
 
 return (grid);
