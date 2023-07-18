43dff071b79094e168267f82110ab7c4fcb33885
benzoic09 <benzoic09@gmail.com>
2023-07-18 09:27:59 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,33 +10,40 @@
  * @owner: pointer to the name of the owner
  * Return: pointer to the newly created dog, or NULL if it fails
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
+	char *new_name, *new_owner;
+
 if (name == NULL || owner == NULL)
 return (NULL);
 
-dog_t *new_dog = malloc(sizeof(dog_t));
+new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
 
-new_dog->name = malloc(strlen(name) + 1);
-if (new_dog->name == NULL)
+new_name = malloc(strlen(name) + 1);
+if (new_name == NULL)
+
 {
 free(new_dog);
 return (NULL);
 }
-strcpy(new_dog->name, name);
+strcpy(new_name, name);
 
-new_dog->owner = malloc(strlen(owner) + 1);
-if (new_dog->owner == NULL)
+new_owner = malloc(strlen(owner) + 1);
+if (new_owner == NULL)
 {
-free(new_dog->name);
+free(new_name);
 free(new_dog);
 return (NULL);
 }
-strcpy(new_dog->owner, owner);
+strcpy(new_owner, owner);
 
+new_dog->name = new_name;
 new_dog->age = age;
+new_dog->owner = new_owner;
 
 return (new_dog);
 }
