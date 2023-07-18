e241bf9bf43d9de7922cbae17902b265e4dc9603
benzoic09 <benzoic09@gmail.com>
2023-07-18 05:13:53 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - main function
+ * @d: pointer to structure
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+	return;
+
+printf("Name: %s\n", (d.name != NULL) ? d->name : "(nil)");
+printf("Age: %.1f\n", d->age);
+printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+}
