22b9a87d2dcfa995a8466ec810a25da3476d6634
benzoic09 <benzoic09@gmail.com>
2023-07-19 08:43:54 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,16 +10,17 @@
  *         or -1 if no element matches or size <= 0
  */
 
-int int.index(int *array, int size, int (*cmp)(int)
+int.index(int *array, int size, int (*cmp)(int)
 		{
 		int i;
+
 		if (array == NULL || cmp == NULL || size <= 0)
-		return(-1);
+		return (-1);
 
 		for (i = 0; i < size; i++)
 		{
 		if (cmp(array[i]))
-		return(i);
+		return (i);
 		}
-		return(-1);
+		return (-1);
 		}
