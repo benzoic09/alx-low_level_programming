47219059532f280930e9bc43cb8a074d0cc176d6
benzoic09 <benzoic09@gmail.com>
2023-07-27 14:12:47 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,23 +17,25 @@ unsigned int len = 0;
 while (str[len])
 len++;
 
-
+/**
 if (str == NULL)
 return (NULL);
-
+*/
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
 
 new_node->str = strdup(str);
+/**
 if (new_node->str == NULL)
 {
 free(new_node);
 return (NULL);
 }
-
+*/
 new_node->next = *head;
 *head = new_node;
+
 
 return (new_node);
 }
