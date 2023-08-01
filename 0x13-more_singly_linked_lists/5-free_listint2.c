a2a0a65433e5b11eb2b73abc77d73b1125ae40dc
benzoic09 <benzoic09@gmail.com>
2023-08-01 03:15:00 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,16 +5,18 @@
  * @head: listint_t list to be freed
  */
 
-void free_listint2(listint_t **head) 
+void free_listint2(listint_t **head)
 {
-listint_t *current = *head;
-listint_t *next;
-    
-while (current != NULL) {
-next = current->next;
-free(current);
-current = next;
-}
 
-*head = NULL;
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head = NULL;
 }
