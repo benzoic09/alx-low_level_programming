942261c16cc39220dbeb3baf135cd6c5fba23a85
benzoic09 <benzoic09@gmail.com>
2023-08-01 02:37:25 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#incude "lists.h"
+
+
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
