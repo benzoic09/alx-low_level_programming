f996934de09f574f8e8b17fe6bcda7e4d925a939
benzoic09 <benzoic09@gmail.com>
2023-08-01 01:49:02 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,11 +19,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	/*if (*head == NULL)
+	if (*head == NULL)
 	{
-		*head == new_node;
+		*head = new_node;
 		return (new_node);
-	}*/
+	}
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
