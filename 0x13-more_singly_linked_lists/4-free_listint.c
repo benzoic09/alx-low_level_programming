2ebcbbecd3dd6281248ccd31a9a55e3b833f9a09
benzoic09 <benzoic09@gmail.com>
2023-08-01 02:39:33 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,10 @@
-#incude "lists.h"
+#include "lists.h"
 
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ * return: free nodes
+ */
 
 void free_listint(listint_t *head)
 {
