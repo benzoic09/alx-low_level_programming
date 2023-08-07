dfe04a87a997efff8fbaa2fc5526d52ff3bcb67e
benzoic09 <benzoic09@gmail.com>
2023-08-07 09:42:59 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <fcntl.h>
-
+#include <string.h>
 
 /**
  * create_file - Creates a file.
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_des = open(filename, O_WRONLY | O_CREAT | O_TUNC, S_IRUSR | S_IWUSR);
+	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_des == -1)
 	return (-1);
 
@@ -27,12 +27,12 @@ int create_file(const char *filename, char *text_content)
 		_write = write(file_des, text_content, strlen(text_content));
 		if (_write == -1)
 		{
-			close(feile_des);
+			close(file_des);
 			return (-1);
 		}
 	}
 
-	close(file_des)
+	close(file_des);
 		return (-1);
 }
 
