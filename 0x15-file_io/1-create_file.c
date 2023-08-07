519dc43a708ad5c304abbb9dc7d97911cede6553
benzoic09 <benzoic09@gmail.com>
2023-08-07 11:20:44 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,11 +30,8 @@ int create_file(const char *filename, char *text_content)
 			len++;
 
 		_write = write(file_des, text_content, len);
-		if (_write == -1)
-		{
-			close(file_des);
+		if (_write == -1 || file_des == -1)
 			return (-1);
-		}
 	}
 
 	close(file_des);
