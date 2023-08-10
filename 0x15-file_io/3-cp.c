e4e0d3df972fe30a3d2ce6202d39d7f41be714e9
benzoic09 <benzoic09@gmail.com>
2023-08-10 07:12:51 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,50 +3,72 @@
 
 #define BUFFER_SIZE 1024
 
+void error_exit(const char *message, int exit_code);
+int copy_file(const char *source, const char *destination);
+void print_copied_bytes(ssize_t bytes);
+
 /**
- * error_exit - prints the provided error message followed by a newline.
- * 
- * Return: N/A
+ * main - Entry point of the program.
+ * @argc: The number of command line arguments.
+ * @argv: An array of strings containing the arguments.
+ * Return: 0 on success, 97 on argument count mismatch, exit code from copy_file on error.
  */
-
-void error_exit(const char *message, int exit_code)
-{
-    dprintf(STDERR_FILENO, "%s\n", message);
-    exit(exit_code);
-}
-
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to;
-ssize_t n_read, n_write;
-char buffer[BUFFER_SIZE];
-
 if (argc != 3)
 {
 error_exit("Usage: cp file_from file_to", 97);
 }
 
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+int result = copy_file(argv[1], argv[2]);
+
+return (result);
 }
 
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+/**
+ * error_exit - Prints the provided error message followed by a newline and exits.
+ * @message: The error message to print.
+ * @exit_code: The exit code to use when exiting.
+ */
+void error_exit(const char *message, int exit_code)
+{
+dprintf(STDERR_FILENO, "%s\n", message);
+exit(exit_code);
+}
+
+/**
+ * copy_file - Copies the content of one file to another.
+ * @source: The source file to read from.
+ * @destination: The destination file to write to.
+ * Return: 0 on success, 98 if read error, 99 if write error.
+ */
+int copy_file(const char *source, const char *destination)
+{
+int fd_from, fd_to;
+ssize_t n_read, n_write;
+char buffer[BUFFER_SIZE];
+
+fd_from = open(source, O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
+return (98);
+}
+
+fd_to = open(destination, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (fd_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination);
 close(fd_from);
-exit(99);
+return (99);
 }
 
 if (fchmod(fd_to, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't set permissions for %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't set permissions for %s\n", destination);
 close(fd_from);
 close(fd_to);
-exit(99);
+return (99);
 }
 
 while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
@@ -54,33 +76,30 @@ while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 n_write = write(fd_to, buffer, n_read);
 if (n_write != n_read)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination);
 close(fd_from);
 close(fd_to);
-exit(99);
+return (99);
 }
+print_copied_bytes(n_write);
 }
 
-if (n_read == -1) 
+if (n_read == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
 close(fd_from);
 close(fd_to);
-exit(98);
+return (98);
 }
 
-if (close(fd_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-exit(100);
-}
-
-if (close(fd_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
-}
-
+close(fd_from);
+close(fd_to);
 return (0);
+}
+
+
+void print_copied_bytes(ssize_t bytes)
+{
+printf("Copied %zd bytes\n", bytes);
 }
 
