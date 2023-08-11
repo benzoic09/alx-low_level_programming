738f54fe933cab7c7d05adf0996c579b033e6437
benzoic09 <benzoic09@gmail.com>
2023-08-11 10:37:43 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,7 +22,7 @@ void error_exit(const char *message, int exit_code)
  */
 int main(int argc, char *argv[])
 {
-	int fd_source, fd_destination;
+	int fd_from, fd_to;
 	ssize_t n_read, n_write;
 	char buffer[BUFFER_SIZE];
 
@@ -31,29 +31,29 @@ int main(int argc, char *argv[])
 		error_exit("Usage: copy_program source_file destination_file", 97);
 	}
 
-	fd_source = open(argv[1], O_RDONLY);
-	if (fd_source == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	fd_destination = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd_destination == -1)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_source);
+		close(fd_from);
 		exit(99);
 	}
 
-	while ((n_read = read(fd_source, buffer, BUFFER_SIZE)) > 0)
+	while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		n_write = write(fd_destination, buffer, n_read);
+		n_write = write(fd_to, buffer, n_read);
 		if (n_write != n_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_source);
-			close(fd_destination);
+			close(fd_from);
+			close(fd_to);
 			exit(99);
 		}
 	}
@@ -61,18 +61,18 @@ int main(int argc, char *argv[])
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_source);
-		close(fd_destination);
+		close(fd_from);
+		close(fd_to);
 		exit(98);
 	}
 
-	if (close(fd_source) == -1)
+	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
-	if (close(fd_destination) == -1)
+	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
