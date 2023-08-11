366e8a58feda87d1a6033ccb7b9efd6416acc900
benzoic09 <benzoic09@gmail.com>
2023-08-11 10:20:49 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,7 +33,6 @@ int main(int argc, char *argv[])
 
 	fd_source = open(argv[1], O_RDONLY);
 	if (fd_source == -1)
-	return (-1);
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -41,7 +40,6 @@ int main(int argc, char *argv[])
 
 	fd_destination = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_destination == -1)
-	return (-1);
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_source);
@@ -70,14 +68,14 @@ int main(int argc, char *argv[])
 
 	if (close(fd_source) == -1)
 	{
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-        exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
 	}
 
 	if (close(fd_destination) == -1)
 	{
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-        exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
 	}
 
 	close(fd_source);
