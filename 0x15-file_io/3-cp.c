08e63b52451877193182e0d516105a419d2f8fa6
benzoic09 <benzoic09@gmail.com>
2023-08-11 09:26:14 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,17 +23,51 @@ void error_exit(const char *message, int exit_code)
  */
 int main(int argc, char *argv[])
 {
+	int fd_source, fd_destination;
+	ssize_t n_read, n_write;
+	char buffer[BUFFER_SIZE];
+
 	if (argc != 3)
 	{
 		error_exit("Usage: copy_program source_file destination_file", 97);
 	}
 
-	if (copy_file(argv[1], argv[2]) == -1)
+	fd_source = open(source, O_RDONLY);
+	if (fd_source == -1)
+	return (-1);
 	{
-		error_exit("Error: Copy failed", 98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
-	printf("File copied successfully!\n");
+	fd_destination = open(destination, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
+	return (-1);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_source);
+		exit(99);
+	}
 
+	while ((n_read = read(fd_source, buffer, BUFFER_SIZE)) > 0)
+	{
+		n_written = write(fd_destination, buffer, n_read);
+		if (n_written != n_read)
+		{
+			close(fd_source);
+			close(fd_destination);
+			return (-1);
+		}
+	}
+
+	if (n_read == -1)
+	{
+		close(fd_source);
+		close(fd_destination);
+		return (-1);
+	}
+
+	close(fd_source);
+	close(fd_destination);
 	return (0);
 }
