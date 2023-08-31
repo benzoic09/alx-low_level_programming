66cc4270e8eab848d64d8aa9ea3f70d65b80e896
Ben Kimani <benzoic09@gmail.com>
2023-08-31 12:39:53 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,9 +2,9 @@
 
 /**
  * flip_bits - returns the number of bits neededto flip
- * @n - the first number
- * @m - the second number
- * Return - the number of bits to flip
+ * @n: - the first number
+ * @m: - the second number
+ * Return: - the number of bits to flip
  */
 
 unsigned int flip_bits(unsigned int n, unsigned long int m)
