8b3944bd3816340a3f32953357e258256eb28837
Ben Kimani <benzoic09@gmail.com>
2023-08-31 09:16:35 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * pint_binary - prints the binary rep of a no.
+ * @n: - No to convert and print binary
+ *
+ * return - binay repreesantation
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
+}
