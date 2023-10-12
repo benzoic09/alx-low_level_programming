117fae3339c97da841e66b74a1c6163d01e509c8
Ben Kimani <benzoic09@gmail.com>
2023-10-12 10:41:07 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,7 +9,7 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count =0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
@@ -18,5 +18,5 @@ size_t print_dlistint(const dlistint_t *h)
 		count++;
 	}
 
-	return count;
+	return (count);
 }
