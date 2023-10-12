3ab7bfc37a2e9e11d4cbf756057cc92e5541dd90
Ben Kimani <benzoic09@gmail.com>
2023-10-12 14:17:47 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (current != NULL)
 	{
-		if (current_index == idx - 1)
+		if (current_index == (idx - 1))
 		{
 			new_node->next = current->next;
 			new_node->prev = current;
