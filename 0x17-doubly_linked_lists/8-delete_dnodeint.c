b706ea8070675a4eba435bc54865f4edd54394c2
Ben Kimani <benzoic09@gmail.com>
2023-10-13 05:50:50 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (current->next != NULL)
 			current->next->prev = NULL;
 		free(current);
-		return (-1);
+		return (1);
 	}
 
 	while (current != NULL)
@@ -32,7 +32,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (current->next != NULL)
 				current->next->prev = current->prev;
 			free(current);
-			return (-1);
+			return (1);
 		}
 
 	current = current->next;
