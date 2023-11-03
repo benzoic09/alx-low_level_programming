caf6f01d3c92b68a9ddb691e8a7d3838d265bebf
Ben Kimani <benzoic09@gmail.com>
2023-11-03 11:33:40 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,11 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
-
 	temp = ht->array[index];
-
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
@@ -33,7 +30,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 		temp = temp->next;
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
@@ -51,9 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
