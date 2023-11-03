37ab5f28ea444cc19763ba2a3af8833740a89f18
Ben Kimani <benzoic09@gmail.com>
2023-11-03 10:41:16 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = temp->next;
 	}
 
-	new_node = malloc(sizeof(hashnode_t));
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 
