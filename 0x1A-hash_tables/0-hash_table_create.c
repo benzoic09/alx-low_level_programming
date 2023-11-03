fdb25a38422da3f50e49d0554f12caad1c35a9e0
Ben Kimani <benzoic09@gmail.com>
2023-11-03 05:44:13 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
-	if (size < 1)
+	if (size == 0)
 	return (NULL);
 
 	new_table = malloc(sizeof(hash_table_t));
