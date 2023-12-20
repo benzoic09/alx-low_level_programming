507d6a316de02950693334c66d6995a0db756487
Ben Kimani <benzoic09@gmail.com>
2023-12-20 10:24:46 -0800

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,21 +7,22 @@ Constraints:
     - One cell is a square with a side length of 1.
     - Grid cells are connected horizontally/vertically (not diagonally).
     - Grid is rectangular, with width and height not exceeding 100.
-    - Grid is completely surrounded by water, with only one island (or nothing).
-    - The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+    - Grid is completely surrounded by water,
+        with only one island (or nothing).
+    - The island doesn’t have “lakes”
+        (water inside that isn’t connected to the water around the island).
     """
 
     rows = len(grid)
     cols = len(grid[0]) if rows else 0
     perimeter = 0
 
-
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
 
-                if i > 0 and grid[i -1][j] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
 
                 if i < rows - 1 and grid[i + 1][j] == 1:
@@ -34,5 +35,3 @@ Constraints:
                     perimeter -= 1
 
     return perimeter
-
-
