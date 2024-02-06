f25661ff26a66188d6ea535b7b527754a8a10611
Ben Kimani <benzoic09@gmail.com>
2024-02-06 04:30:09 -0800

    binary_search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (left <= righti)
+	while (left <= right)
 	{
 		mid = left + (right - left) / 2;
 
